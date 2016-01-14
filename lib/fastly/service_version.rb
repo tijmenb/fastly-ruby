078695447a096f4da3a7f5ebcd2350c066e08ed0
Josh Lane <me@joshualane.com>
2016-01-14 09:38:07 -0800

    service version detail attributes

--- lib/fastly/service_version.rb
+++ lib/fastly/service_version.rb
@@ -2,17 +2,32 @@ class Fastly::ServiceVersion < Fastly::Model
 
   identity :id, alias: "number", type: :integer
 
-  attribute :active,     type: :boolean #   Whether this is the active version or not.
+  attribute :active,           type: :boolean # Whether this is the active version or not.
+  attribute :backends,         type: :array
+  attribute :cache_settings,   type: :array
   attribute :comment
-  attribute :created_at, type: :time
-  attribute :deleted_at, type: :time
-  attribute :deployed,   type: :boolean
-  attribute :locked,     type: :boolean # Whether this version is locked or not. Objects can not be added or edited on locked versions.
-  attribute :number,     type: :integer #  The number of this version.
+  attribute :conditions,       type: :array
+  attribute :created_at,       type: :time
+  attribute :deleted_at,       type: :time
+  attribute :deployed,         type: :boolean
+  attribute :directors,        type: :array
+  attribute :domains,          type: :array
+  attribute :gzips,            type: :array
+  attribute :headers,          type: :array
+  attribute :healthchecks,     type: :array
+  attribute :locked,           type: :boolean # Whether this version is locked or not. Objects can not be added or edited on locked versions.
+  attribute :matches,          type: :array
+  attribute :number,           type: :integer # The number of this version.
+  attribute :origins,          type: :array
+  attribute :request_settings, type: :array
+  attribute :response_objects, type: :array
   attribute :service_id
-  attribute :staging,    type: :boolean
-  attribute :testing,    type: :boolean
-  attribute :updated_at, type: :time
+  attribute :settings
+  attribute :staging,          type: :boolean
+  attribute :testing,          type: :boolean
+  attribute :updated_at,       type: :time
+  attribute :vcls,             type: :array
+  attribute :wordpress,        type: :array
 
   def service
     @service ||= client.services.get(service_id)
